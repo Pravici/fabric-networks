10a2663de4d550d842898a2ec3a0981d0ab98257
Jason Rasmussen <jason@pravici.com>
2020-07-22 16:16:56 -0700

    feat: wallet in a separate container

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ fi
 
 COMPOSE_FILENAME="docker-compose.$TARGET_ENV.yml"
 docker-compose -f $COMPOSE_FILENAME down -v
-docker-compose -f $COMPOSE_FILENAME up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb cli
+docker-compose -f $COMPOSE_FILENAME up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb wallet cli
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -19,7 +19,7 @@ export FABRIC_START_TIMEOUT=10
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-curl -u "tlpstateuser:ufgVk0IP3Yh4ORJlzaRB4oCiGsPZJ2tb5fy6g4XW90I=" -X PUT http://127.0.0.1:5050/wallet
+curl -u "tlpstateuser:ufgVk0IP3Yh4ORJlzaRB4oCiGsPZJ2tb5fy6g4XW90I=" -X PUT http://127.0.0.1:5051/wallet
 
 create_demo_channels
 list_channels
